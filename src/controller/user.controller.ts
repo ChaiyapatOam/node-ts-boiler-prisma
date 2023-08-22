e5e60ecba6f11a412483bf012841781078823da5
Chaiyapat Meeying <chaiyapatoam12@gmail.com>
2023-08-22 15:07:20 +0700

    chore: add middleware

--- src/controller/user.controller.ts
+++ src/controller/user.controller.ts
@@ -1,5 +1,5 @@
 import { Request, Response } from "express";
-// import * as userService from "@/services/user.service";
+// import * as userService from "@/service/user.service";
 
 export const test = async (req: Request, res: Response) => {
   res.status(200).send("Hello User!");
--- src/server.ts
+++ src/server.ts
@@ -8,6 +8,8 @@ const port = process.env.PORT || 8080;
 
 app.use(helmet());
 app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.get("/", (req: Request, res: Response) => {
   res.send("Express TypeScript Server is Running");
